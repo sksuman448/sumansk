dafa97c6a4ae2f439a6691dc6d068ff1d19a2cca
Suman SK <60569832+sksuman448@users.noreply.github.com>
2023-04-11 13:46:21 +0530

    Update main.tf

--- terraform-on-aws-repo-main/s3/main.tf
+++ terraform-on-aws-repo-main/s3/main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region = var.aws_region
+ access_key = “AKIAZ64X6ZCI7EDASZGY”
+ secret_key =“4McjUbJDHNlm/zwQGQ3frkpn6Ho22sH8FKPMHrZ2”
+ region= “us-east-2”
 }
+
 
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
