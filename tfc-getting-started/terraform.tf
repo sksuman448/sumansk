b7d4c490485d8344e1b739e45543982dba74d42b
Suman SK <60569832+sksuman448@users.noreply.github.com>
2023-04-13 14:03:43 +0530

    Add files via upload

---
+++ tfc-getting-started/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  cloud {
+    organization = "suman123"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+}
